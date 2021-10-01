537284eda59424b29ddcab75a253cc8f958eac5a
Igor <igormax2008@gmail.com>
2021-10-01 18:59:44 -0300

    Aula 17 - Exemplo de teste de command

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.IsValid);
+        }
+    }
+}
