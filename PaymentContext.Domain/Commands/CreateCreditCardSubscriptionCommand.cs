c05aa25e91662a2e892d4556b5c8be64725ea51c
Igor <igormax2008@gmail.com>
2021-10-01 18:29:15 -0300

    Aula 15 - CQRS

---
+++ PaymentContext.Domain/Commands/CreateCreditCardSubscriptionCommand.cs
@@ -0,0 +1,37 @@
+using System;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateCreditCardSubscriptionCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        public string CardHolderName { get; private set; }
+        public string CardNumber { get; private set; }
+        public string LastTransactionNumber { get; private set; }
+
+        public string Email { get; set; }
+        public string TransactionCode { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+
+        public string Payer { get; set; }
+        public string PayerDocument { get; set; }
+        public EDocumentType PayerDocumentType { get; set; }
+        public string PayerAddress { get; set; }
+
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string Neighborhood { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public string Country { get; private set; }
+        public string ZipCode { get; private set; }
+        
+    }
+}
